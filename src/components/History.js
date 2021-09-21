e48d910d84e5a88f2c4aa3236af8a1664b5c7b73
jerco2 <jericogodoy7296@gmail.com>
2021-09-21 13:57:56 +0800

    change History directory

--- src/components/History.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import HistoryMoves from "./HistoryMoves";
-
-function History({ history }) {
-  return history.map((move) => <HistoryMoves key={move.id} moves={move} />);
-}
-
-export default History;
